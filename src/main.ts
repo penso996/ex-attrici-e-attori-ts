71d5fb05bf6f856fde6a1a75750263940f8d6796
penso996 <penso.andrea@outlook.it>
2025-07-09 12:35:16 +0200

    milestone 3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 type Person = {
   readonly id: number,
   readonly name: string,
-  birthYear: number,
-  deathYear?: number,
+  birth_year: number,
+  death_year?: number,
   biography: string,
   image: string
 }
@@ -21,7 +21,42 @@ type ActressNationality =
   | "Chinese"
 
 type Actress = Person & {
-  mostFamousMovie: [string, string, string],
+  most_famous_movie: [string, string, string],
   awards: string,
   nationality: ActressNationality,
+}
+
+function isActress(dati: unknown): dati is Actress {
+  return (
+    typeof dati === "object" && dati !== null &&
+    "id" in dati && typeof dati.id === "number" &&
+    "name" in dati && typeof dati.name === "string" &&
+    "birth_year" in dati && typeof dati.birth_year === "number" &&
+    "death_year" in dati && typeof dati.death_year === "number" &&
+    "biography" in dati && typeof dati.biography === "string" &&
+    "image" in dati && typeof dati.image === "string" &&
+    "most_famous_movie" in dati
+    && dati.most_famous_movie instanceof Array
+    && dati.most_famous_movie.length === 3
+    && dati.most_famous_movie.every(m => typeof m === "string") &&
+    "awards" in dati && typeof dati.awards === "string" &&
+    "nationality" in dati && typeof dati.nationality === "string"
+  )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    const dati: unknown = await response.json();
+    if (!isActress(dati)) {
+      throw new Error("Formato dati non valido");
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore durante il recupero dell'attrice", error);
+    } else {
+      console.error("Errore sconosciuto", error);
+    }
+    return null;
+  }
 }
